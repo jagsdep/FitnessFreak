6dc3857f4c6e1d5316d31f814def5fc9d40c01d5
Jagruti <jagruti.depan@gmail.com>
2021-11-30 12:36:00 -0600

    changes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-// require('dotenv').config()
+
  const express = require('express')
+ const cors = require('cors')
  const app = express()
  const path = require('path');
  
  app.use(express.json())
+ app.use(cors())
  
  app.use(express.static("public"));
  app.use(express.urlencoded({ extended: true }));
